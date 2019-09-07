ee07150836b428c5fc3eabadaee43877c250a519
Đặng Xuân Vương <vuong.1998.school.01@gmail.com>
2019-09-07 11:59:35 +0700

    Add `create order form`

--- common/utils/vinid-json.js
+++ common/utils/vinid-json.js
@@ -1,35 +1,44 @@
 
 
 module.exports = {
-    createJson: async function() {
+    createJson: function() {
         return {
-            "data": {
-                "metadata": {
-                    "app_name": "bitZero",
-                    "title": "Ứng dụng mua hàng tự động cùng bitZero",
-                    "submit_button": {
-                        "label": "Gửi thông tin",
-                        "background_color": "#6666ff",
-                        "cta": "request",
-                        "url": "https://teambit.tech/api/send-response"
-                    },
-                    "reset_button": {
-                        "label": "Xoá bản ghi",
-                        "background_color": "#669999"
-                    },
-                    "elements": []
-                }
+            "metadata": {
+                "app_name": "bitZero",
+                "title": "bitZero - Tự định nghĩa cách bạn mua hàng",
+                "submit_button": {
+                    "label": "Gửi thông tin",
+                    "background_color": "#6666ff",
+                    "cta": "request",
+                    "url": "https://teambit.tech/api/send-response"
+                },
+                "reset_button": {
+                    "label": "Xoá bản ghi",
+                    "background_color": "#669999"
+                },
+                "elements": []
             }
         }
     },
     
-    addElement: async function(json, element) {
-        json.data.metadata.element.add(element);
+    addElement: function(json, element) {
+        json.metadata.elements.push(element);
     },
 
-    addElements: async function(json, elements) {
-        for (element in elements) {
-            this.addElements(json, element);
+    addElements: function(json, elements) {
+        for (i in elements) {
+            element = elements[i]
+            this.addElement(json, element);
         }
+    },
+
+    actionToElement: function(action) {
+        // console.log(action)
+        var element = {}
+        element.type = 'text';
+        element.style = 'paragraph'
+        element.content = action.actionName
+        // console.log(element)
+        return element
     }
 }
